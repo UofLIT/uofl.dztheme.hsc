8ab9c00339db1a6ffeb5f5e5d3498994d78a1381
Derek Davenport <derekdavenport@gmail.com>
2016-09-23 15:15:19 -0400

    1.0.6
    Added image captioning and "full size" viewing. Images must have class
    "caption" and have an alt or title attribute.

--- uofl.dztheme.hsc/js/theme.js
+++ uofl.dztheme.hsc/js/theme.js
@@ -53,35 +53,69 @@ jqbs(function($){
 		swipe: true
 	};
 
-	$heroWrapper = $('.med-hero-wrapper');
-	if($heroWrapper.is(':empty')) {
-		$.ajax({
-			type:     'POST',
-			url:      '../images/banners/tinymce-jsonimagefolderlisting',
-			data:     'rooted=True&document_base_url=/',
-			dataType: 'json'
-		}).done(function (json) {
-			var images = $.grep(json.items, function (item) {
-					return item.portal_type === 'Image';
-			});
-			$.each(images, function (i, image) {
-				var $slide = $('<div class="slide slide-bg slide-minimal" style="background-image: url(' + image.url + ');"><h1><span>' + image.title + '</span></h1></div>');
-				$.get(image.url + '/Description', function (result) {
-					var lines = result.split('\n');
-					if(lines[0]) $slide.append('<div class="description"><div class="container">' + marked(lines[0]) + '</div></div>');
-					if(lines[1]) $slide.css({backgroundPosition: lines[1]});
+	/*
+	 * automatically build the hero slider from the images/banners folder or use the existing HTML
+	 */
+	if ($heroWrapper = $('.med-hero-wrapper')) {
+		if ($heroWrapper.is(':empty')) {
+			$.ajax({
+				type:     'POST',
+				url:      '../images/banners/tinymce-jsonimagefolderlisting',
+				data:     'rooted=True&document_base_url=/',
+				dataType: 'json'
+			}).done(function (json) {
+				var images = $.grep(json.items, function (item) {
+						return item.portal_type === 'Image';
 				});
-				$heroWrapper.append($slide);
+				$.each(images, function (i, image) {
+					var $slide = $('<div class="slide slide-bg slide-minimal" style="background-image: url(' + image.url + ');"><h1><span>' + image.title + '</span></h1></div>');
+					$.get(image.url + '/Description', function (result) {
+						var lines = result.split('\n');
+						if(lines[0]) $slide.append('<div class="description"><div class="container">' + marked(lines[0]) + '</div></div>');
+						if(lines[1]) $slide.css({backgroundPosition: lines[1]});
+					});
+					$heroWrapper.append($slide);
+				});
+			
+				$heroWrapper.carouFredSel(carouFredSelOptions);
 			});
-		
+		}
+		else {
 			$heroWrapper.carouFredSel(carouFredSelOptions);
-		});
-	}
-	else {
-		$heroWrapper.carouFredSel(carouFredSelOptions);
+		}
 	}
 
-	
+	/*
+	 * look for images with caption class and display the title (or alt as backup) as the caption
+	 * if the image is scaled down, give option to click for full size
+	 */
+	$('img.caption[title][title!=""], img.caption[alt][alt!=""]').each(function () {
+		var $this = $(this);
+		var captionText = $.trim($this.attr('title')) || $.trim($this.attr('alt'));
+		if (!captionText) return;
+		var $figure = $('<figure>').addClass($this.attr('class'));
+		$this.removeAttr('class');
+		var $figcaption = $('<figcaption>').text(captionText);
+		
+		$figure.insertBefore($this).append($this).append($figcaption);
+		var match, $modal;
+		if (match = $this.attr('src').match(/(.*)\/(image_\w+|@@images\/.*)\/?$/)) {
+			$this.addClass('click').on('click', function () {
+				if (!$modal) {
+					$modal = $('<div class="modal hide fade">').append(
+						$('<div class="modal-header">').append(
+							'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>',
+							$('<h3>').text(captionText)
+						),
+						$('<div class="modal-body">').append(
+							$('<img/>').attr('src', match[1])
+						)
+					).insertAfter($figure);
+				}
+				$modal.modal();
+			});
+		}
+	});
 
 	$(document).on('click', '.med-nav-toggle, .med-nav-pane-collapse a, .upperbar-accounts-collapse, .tab-pane-collapse a', function(e) {
 		e.stopPropagation();
